4fb0eb15bdc23f097265f008d6307e1adf0bb26c
Deanna Hood <deanna.m.hood@gmail.com>
2014-09-07 08:38:25 +0200

    BroadcastReceiver for creating notification when alarm goes off

---
+++ app/src/main/java/com/example/deanna/dailyimdbquotes/AlarmReceiver.java
@@ -0,0 +1,29 @@
+package com.example.deanna.dailyimdbquotes;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by deanna on 8/30/14.
+ */
+public class AlarmReceiver extends BroadcastReceiver
+{
+    private static final String LOG_TAG = AlarmReceiver.class.getName();
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+        MyAlarmManager.createNotification(context);
+
+
+        if(MainActivity.instance != null){
+            MainActivity.instance.displayNotificationWaiting();
+        }
+        Log.d(LOG_TAG, "Alarm received");
+
+    }
+
+}
